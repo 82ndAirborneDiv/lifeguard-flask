6556d73035e8a99b327197f571495e524bb60d2d
Greg Ledbetter <gsledbetter@gmail.com>
2015-04-09 10:37:54 -0400

    Update Jacob.

--- lifeguard.py
+++ lifeguard.py
@@ -7,7 +7,7 @@ import pytz
 app = Flask(__name__)
 device_owners = {
     'BFFE2480-0A6A-485A-8056-8EA2FDF3EFC4': 'Greg Ledbetter',
-    '9D2A19B1-4E04-4BAA-87F4-13C0A8DACC97': 'Jacob Ledbetter',
+    'A6AF418B-B6D3-44DF-A634-7F21E42F8496': 'Jacob Ledbetter',
     '043E6CF8-C979-44EE-8266-644CDF93B902': 'Nick Ledbetter',
     'ED33EFF5-04B3-4458-A5CF-64E774540AB9': 'Pam Ledbetter'
 
